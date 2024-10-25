1e703cad9e893e3f80b3fea4d7b424e341b45fad
Emanuele Bianchi <emanuelebianchi00@gmail.com>
2024-10-25 09:32:42 +0200

    Update alert Welcome Page

--- src/Welcome Page/assets/js/welcome.js
+++ src/Welcome Page/assets/js/welcome.js
@@ -1,10 +1,17 @@
 const btn = document.querySelector("button");
 const checkBox = document.getElementById("check1");
+const alertComment = document.getElementById("alertComment");
 
 btn.onclick = () => {
   if (checkBox.checked) {
     window.open("/src/Test Page/test.html", "_self");
   } else {
-    alert("Flaggare la checkbox per continuare");
+    alertComment.innerText = "WARNING! Check the box to continue";
+    alertComment.style.color = "red";
+    alertComment.classList.remove("hide");
   }
+};
+
+checkBox.onclick = () => {
+  alertComment.classList.add("hide");
 };
